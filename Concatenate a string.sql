8fee2ca136ffa9f94750efaa0d16a5e2c43f4a64
riteshksinha <ritesh.sinha524@gmail.com>
2018-06-27 14:59:17 -0700

    Update Concatenate a string.sql

--- Concatenate a string.sql
+++ Concatenate a string.sql
@@ -6,3 +6,33 @@ SELECT DISTINCT T1.ID,
              FOR XML PATH (''))
           , 1, 1, '')  AS CONCATENATED_DATE
 FROM TABLENAME T1;
+
+/**********************************************************/
+
+SELECT MAIN.SUBJECTID,
+       LEFT(MAIN.STUDENTS,LEN(MAIN.STUDENTS)-1) AS "STUDENTS"
+FROM
+    (
+        SELECT DISTINCT ST2.SUBJECTID, 
+            (
+                SELECT ST1.STUDENTNAME + ',' AS [TEXT()]
+                FROM DBO.STUDENTS ST1
+                WHERE ST1.SUBJECTID = ST2.SUBJECTID
+                ORDER BY ST1.SUBJECTID
+                FOR XML PATH ('')
+            ) [STUDENTS]
+        FROM DBO.STUDENTS ST2
+    ) [MAIN]
+    
+ /**********************************************************/
+ 
+ SELECT DISTINCT ST2.SUBJECTID, 
+    SUBSTRING(
+        (
+            SELECT ','+ST1.STUDENTNAME  AS [TEXT()]
+            FROM DBO.STUDENTS ST1
+            WHERE ST1.SUBJECTID = ST2.SUBJECTID
+            ORDER BY ST1.SUBJECTID
+            FOR XML PATH ('')
+        ), 2, 1000) [STUDENTS]
+FROM DBO.STUDENTS ST2
